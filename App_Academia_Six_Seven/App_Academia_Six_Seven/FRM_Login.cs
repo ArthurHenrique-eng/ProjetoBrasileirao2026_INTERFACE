9e77517250282f02d1b84f0f59847001b98e80bc
Arthur H. <arthurhpb7@icloud.com>
2026-02-27 21:34:18 -0300

    SENAC LOGIN

---
+++ App_Academia_Six_Seven/App_Academia_Six_Seven/FRM_Login.cs
@@ -0,0 +1,15 @@
+namespace App_Academia_Six_Seven
+{
+    public partial class FRM_Login : Form
+    {
+        public FRM_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
